f3a04c84b22ff3ed7d7d0ec7332c567589d83059
eliasinacio <el1as.1nac1o.ch@gmail.com>
2021-05-28 14:44:37 -0300

    modal finished. Info and price placed.

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const Wines = {
       grapes: ["Pinot Gris", "Pinot Noir"],
       harvest: "2012",
 
-      alcohol: 12
+      alcohol: 12,
+      price: 129.00
     },
     {
       name: "Pure Galaxy",
@@ -18,7 +19,8 @@ const Wines = {
       grapes: ["Nebbiolo", "Grenache"],
       harvest: "2011",
 
-      alcohol: 14
+      alcohol: 14,
+      price: 68.50
     },
     {
       name: "Black Mirror",
@@ -28,21 +30,46 @@ const Wines = {
       grapes: ["Pinot Meunier", "Chardonnay"],
       harvest: "2014",
 
-      alcohol: 20
+      alcohol: 20,
+      price: 87.74
     }
   ]
 }
 
 const modal = document.querySelector('#modal');
+const wineName = document.querySelector('#title h2');
+const wineSub = document.querySelector('#title small');
+const wineWinery = document.querySelector('#winery');
+const wineGrapes = document.querySelector('#grapes');
+const wineHarvest = document.querySelector('#harvest');
+const alcoholValue = document.querySelector('#alcoholValue');
+const alcoholField = document.querySelector('#alcoholField');
+const price = document.querySelector('#price');
+
+
 const Modal = {
+  setValues ( wine ) {
+    wineName.innerText = `${Wines.all[wine].name}`
+    wineSub.innerText = `${Wines.all[wine].flavor}`
+    wineWinery.innerText = `${Wines.all[wine].winery}`
+    wineGrapes.innerText = `${Wines.all[wine].grapes}`
+    wineHarvest.innerText = `${Wines.all[wine].harvest}`
+    alcoholValue.innerText = `${Wines.all[wine].alcohol}`
+    alcoholField.style.width = `${Wines.all[wine].alcohol/10*15+'px'}`
+    price.innerText = `${Wines.all[wine].price.toFixed(2)
+                                .toString()
+                                .replace('.', ",")}`
+  },
   open () {
     modal.style.zIndex = 5;
     modal.hidden = false;
   },
+  
   close () {
     modal.style.zIndex = -1;
     modal.hidden = true;
   }
 }
 
-Modal.open();
+Modal.setValues(1)
+Modal.open()
