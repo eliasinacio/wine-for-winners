2e1a2c40233fa2a6b0231e4105a330a0568a9d7e
eliasinacio <el1as.1nac1o.ch@gmail.com>
2021-05-27 21:54:31 -0300

    modal created. missing: replace values, add price to modals.

---
+++ script.js
@@ -0,0 +1,48 @@
+const Wines = {
+  all: [
+    {
+      name: "Quartz Rosé",
+      flavor: "Red fruits",
+
+      winery: "Luigi Bosca",
+      grapes: ["Pinot Gris", "Pinot Noir"],
+      harvest: "2012",
+
+      alcohol: 12
+    },
+    {
+      name: "Pure Galaxy",
+      flavor: "Orange fruits",
+
+      winery: "Gérard Bertrand",
+      grapes: ["Nebbiolo", "Grenache"],
+      harvest: "2011",
+
+      alcohol: 14
+    },
+    {
+      name: "Black Mirror",
+      flavor: "Red fruits",
+
+      winery: "Kai Simone",
+      grapes: ["Pinot Meunier", "Chardonnay"],
+      harvest: "2014",
+
+      alcohol: 20
+    }
+  ]
+}
+
+const modal = document.querySelector('#modal');
+const Modal = {
+  open () {
+    modal.style.zIndex = 5;
+    modal.hidden = false;
+  },
+  close () {
+    modal.style.zIndex = -1;
+    modal.hidden = true;
+  }
+}
+
+Modal.open();
